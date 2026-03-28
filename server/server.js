4c0a536981c098f5982800c4c5e2df7d79422c9f
dipendrakhadgi <dipendrakhadgi08@gmail.com>
2026-03-28 12:21:39 +0545

    Fix server for Render deployment

--- server/server.js
+++ server/server.js
@@ -51,3 +51,8 @@ if (mode === "menu") {
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
